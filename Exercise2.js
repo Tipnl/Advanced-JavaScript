da9e008e23f220fa9f9fb6bdd77ddd8dc1b2ef2d
Tipnl <siroccowz@hotmail.com>
2023-05-02 21:26:33 +0200

    Day 2 of exercises

---
+++ Exercise2.js
@@ -0,0 +1,35 @@
+// Day 2.
+
+// Exersize 1:
+
+const checkAge = (age) =>
+  age > 18
+    ? true
+    : confirm("Do you have your parents permission to access this page?");
+
+// Exersize 2:
+// I'm not very good with maths, so to be honest this to me is hard to understand even without javascript.
+
+// Exersize 3:
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.")
+);
+
+// Copy over text from exersize, change function() to () =>
+
+// Exersize 4:
+// I do not fully understand objects.
+
+// Exersize 5:
+
+function minOfTwo(a, b) {
+  return a < b ? a : b;
+}
---
+++ Exercise3.js
@@ -0,0 +1,3 @@
+// Sorry, i have worked all evening to solve these problems but decided to call it quits at 21:15.
+// I will continue tomorrow, because i feel like the cirriculum provided does not provide what i need to solve this.
+// And i do currently not have the energy anymore to keep looking after all day continuous work since 9. (12! hours).
